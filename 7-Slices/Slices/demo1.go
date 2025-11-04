07d18bff149d34f091b24675135957c08efd004c
Enes Söğüt <enessogut@outlook.com>
2025-11-04 13:59:11 +0300

    Understanding Slices

---
+++ 7-Slices/Slices/demo1.go
@@ -0,0 +1,17 @@
+package slices
+
+import "fmt"
+
+func Demo1() {
+	names := make([]string, 3)
+
+	fmt.Println(names)
+
+	names[0] = "Engin"
+	names[1] = "Derin"
+	names[2] = "Salih"
+	names = append(names, "Büşra")
+
+	fmt.Println(names)
+
+}
