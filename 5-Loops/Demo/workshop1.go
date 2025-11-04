1d7243126faedc0fdbbb5c4f67f5e37ad44c8bdd
Enes Söğüt <enessogut@outlook.com>
2025-11-04 12:07:26 +0300

    Extras of First Loop Exercise

--- 5-Loops/Demo/workshop1.go
+++ 5-Loops/Demo/workshop1.go
@@ -9,19 +9,29 @@ import (
 
 func Demo3() {
 	target := rand.Intn(100)
-	estimated := 0
+	guess := 0
+	guessCount := 0
 	for i := 0; i < 1; {
 		fmt.Println("Guess a number")
-		fmt.Scanln(&estimated)
+		fmt.Scanln(&guess)
 
-		if estimated == target {
+		if guess == target {
 			fmt.Println("You found the number")
 			i = 1
-		} else if estimated < target {
+		} else if guess < target {
 			fmt.Println("Your guess is low")
 		} else {
 			fmt.Println("Your guess is high")
 		}
+		guessCount++
 	}
 
+	if guessCount <= 3 {
+		fmt.Println("Excellent")
+	} else if guessCount <= 6 {
+		fmt.Println("Good")
+	} else {
+		fmt.Println("Not Bad")
+	}
+	fmt.Println("Guess Count : " + fmt.Sprintf("%v", guessCount))
 }
