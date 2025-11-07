c00b8b69a41783779758cf0e7d4ed26e8212206e
Enes Söğüt <enessogut@outlook.com>
2025-11-07 15:11:00 +0300

    Variadic Functions

--- 8-Functions/main.go
+++ 8-Functions/main.go
@@ -10,9 +10,13 @@ func main() {
 	// functions.Sum(2, 3)
 	// var result = functions.Sum(2, 6)
 	// fmt.Println(result)
-	result1, result2, result3, result4 := functions.FourOperations(10, 6)
-	fmt.Println("Sum :", result1)
-	fmt.Println("Sub :", result2)
-	fmt.Println("Mul :", result3)
-	fmt.Println("Div :", result4)
+	// result1, result2, result3, result4 := functions.FourOperations(10, 6)
+	// fmt.Println("Sum :", result1)
+	// fmt.Println("Sub :", result2)
+	// fmt.Println("Mul :", result3)
+	// fmt.Println("Div :", result4)
+
+	fmt.Println(functions.SumVariadic(1, 4, 6, 3, 10))
+	fmt.Println(functions.SumVariadic(1, 4))
+	fmt.Println(functions.SumVariadic())
 }
